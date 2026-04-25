e394b0e34bd80a8bb869a679b371fac7fe4f9ace
tempel82 <marniek.billiet@gmail.com>
2026-04-25 13:00:02 +0200

    Update controls.js

--- mobile/controls.js
+++ mobile/controls.js
@@ -1,14 +1,56 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const left = document.querySelector('[data-action="p1-left"]');
-    const right = document.querySelector('[data-action="p1-right"]');
 
-    if (left) {
-        left.onclick = () => alert("LEFT klik gezien");
-        left.ontouchstart = () => alert("LEFT touch gezien");
-    }
+    const buttons = document.querySelectorAll("[data-action]");
 
-    if (right) {
-        right.onclick = () => alert("RIGHT klik gezien");
-        right.ontouchstart = () => alert("RIGHT touch gezien");
-    }
+    buttons.forEach(btn => {
+
+        let interval;
+        const action = btn.dataset.action;
+
+        function run() {
+
+            switch (action) {
+
+                case "p1-left":
+                    window.dispatchEvent(new KeyboardEvent("keydown", { key: "a" }));
+                    break;
+
+                case "p1-right":
+                    window.dispatchEvent(new KeyboardEvent("keydown", { key: "d" }));
+                    break;
+
+                case "p1-jump":
+                    window.dispatchEvent(new KeyboardEvent("keydown", { key: "w" }));
+                    break;
+
+                case "p1-punch":
+                    window.dispatchEvent(new KeyboardEvent("keydown", { key: "f" }));
+                    break;
+
+                case "p1-kick":
+                    window.dispatchEvent(new KeyboardEvent("keydown", { key: "g" }));
+                    break;
+
+                case "p1-uppercut":
+                    window.dispatchEvent(new KeyboardEvent("keydown", { key: "h" }));
+                    break;
+
+                case "p1-block":
+                    window.dispatchEvent(new KeyboardEvent("keydown", { key: "r" }));
+                    break;
+            }
+        }
+
+        btn.addEventListener("touchstart", (e) => {
+            e.preventDefault();
+            run();
+            interval = setInterval(run, 120);
+        });
+
+        btn.addEventListener("touchend", () => {
+            clearInterval(interval);
+        });
+
+    });
+
 });
